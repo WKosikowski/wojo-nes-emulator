ff342292109f50350cee4252b35fc02183a25d65
Wojciech Kosikowski <83952389+WKosikowski@users.noreply.github.com>
2025-05-11 17:57:40 +0100

    Test SwiftFormat hook

--- WojoNES/ContentView.swift
+++ WojoNES/ContentView.swift
@@ -7,10 +7,11 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
+struct ContentView: View   {
+    var body:   some View {
         VStack {
-            Image(systemName: "globe")
+            Image(
+                systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
