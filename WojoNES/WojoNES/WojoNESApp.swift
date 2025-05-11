50e63f726d5dd97606012ee17f35dff34e9d9f48
Wojciech Kosikowski <83952389+WKosikowski@users.noreply.github.com>
2025-05-11 16:37:28 +0100

    Created Empty Xcode project

---
+++ WojoNES/WojoNES/WojoNESApp.swift
@@ -0,0 +1,17 @@
+//
+//  WojoNESApp.swift
+//  WojoNES
+//
+//  Created by Wojciech Kosikowski on 11/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct WojoNESApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
