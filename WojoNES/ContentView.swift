de703a24ab3884b680c3b83727651fe0df21baed
Wojciech Kosikowski <83952389+WKosikowski@users.noreply.github.com>
2025-05-11 18:15:54 +0100

    Pre-commit hook test

--- WojoNES/ContentView.swift
+++ WojoNES/ContentView.swift
@@ -7,11 +7,10 @@
 
 import SwiftUI
 
-struct ContentView: View   {
-    var body:   some View {
+struct ContentView: View {
+    var body: some View {
         VStack {
-            Image(
-                systemName: "globe")
+            Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
