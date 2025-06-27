f83b997436eb28731d0ca89e4b4ec0935833f196
Wojciech <83952389+WKosikowski@users.noreply.github.com>
2025-06-27 18:05:52 +0100

    Create Extension for CPU with stub methods (#37)

---
+++ WojoNES/Core/CPU/NESCPU+AddressingModes.swift
@@ -0,0 +1,47 @@
+//
+//  NESCPU+AddressingModes.swift
+//  WojoNES
+//
+//  Created by Wojciech Kosikowski on 27/06/2025.
+//
+
+public extension NESCPU {
+    /// implied
+    func imp() {}
+
+    /// accumulator
+    func acc() {}
+
+    /// immediate
+    func imm() {}
+
+    /// zeroPage
+    func zpg() {}
+
+    /// zeroPageX
+    func zpx() {}
+
+    /// zeroPageY
+    func zpy() {}
+
+    /// relative
+    func rel() {}
+
+    /// absolute
+    func abs() {}
+
+    /// absoluteX
+    func abx() {}
+
+    /// absoluteY
+    func aby() {}
+
+    /// indirect
+    func ind() {}
+
+    /// indirectX
+    func inx() {}
+
+    /// indirectY
+    func iny() {}
+}
