c414b1263e7466f678330388f33a6871dc57b897
Wojciech <83952389+WKosikowski@users.noreply.github.com>
2025-06-27 17:38:02 +0100

    [Task 33] Create NESCPU Class representing the 6502 CPU (#34)
    * Created NESCPU Class with registers
    
    * swiftormat applied

---
+++ WojoNES/Core/CPU/NESCPU.swift
@@ -0,0 +1,51 @@
+//
+//  NESCPU.swift
+//  WojoNES
+//
+//  Created by Wojciech Kosikowski on 27/06/2025.
+//
+
+public final class NESCPU {
+    // MARK: Properties
+
+    /// Formerly known as A.
+    var accumulator: UInt8 = 0 {
+        didSet {
+            setZeroNegativeFlags()
+        }
+    }
+
+    /// Formerly known as X.
+    var xRegister: UInt8 = 0 {
+        didSet {
+            setZeroNegativeFlags()
+        }
+    }
+
+    /// Formerly known as Y.
+    var yRegister: UInt8 = 0 {
+        didSet {
+            setZeroNegativeFlags()
+        }
+    }
+
+    /// Used for unofficial opcodes.
+    var resultRegister: UInt8 = 0 {
+        didSet {
+            setZeroNegativeFlags()
+        }
+    }
+
+    /// Represents the Stack Pointer (SP).
+    var stackPointer: UInt8 = 0
+    /// Represents the Program Counter (PC). Although 6502 uses a 16-bit PC.
+    var programCounter: Int = 0
+
+    // MARK: Lifecycle
+
+    init() {}
+
+    // MARK: Functions
+
+    func setZeroNegativeFlags() {}
+}
