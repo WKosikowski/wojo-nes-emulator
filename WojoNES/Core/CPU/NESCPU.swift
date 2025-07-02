5ff8659591bd08f6be0223516032dbe25e55f0c7
Wojciech <83952389+WKosikowski@users.noreply.github.com>
2025-07-02 19:27:51 +0100

    [Task 46] Implemented step() method to execute  CPU instructions. (#47)

--- WojoNES/Core/CPU/NESCPU.swift
+++ WojoNES/Core/CPU/NESCPU.swift
@@ -5,6 +5,8 @@
 //  Created by Wojciech Kosikowski on 27/06/2025.
 //
 
+// MARK: - NESCPU
+
 public final class NESCPU {
     // MARK: Static Properties
 
@@ -101,5 +103,196 @@ public final class NESCPU {
     func cmp(_ reg: UInt8, _ mem: UInt8) {
         statusRegister.carry = reg >= mem
         resultRegister = UInt8(reg &- mem)
+    }
+}
+
+extension NESCPU {
+    func step() {
+        let opcode = read(programCounter)
+        programCounter += 1
+        let operation = operations[Int(opcode)]
+        switch operation.addressingMode {
+            case .implied:
+                imp()
+            case .immediate:
+                imm()
+            case .zeroPage:
+                zpg()
+            case .zeroPageX:
+                zpx()
+            case .zeroPageY:
+                zpy()
+            case .relative:
+                rel()
+            case .absolute:
+                abs()
+            case .absoluteX:
+                abx()
+            case .absoluteY:
+                aby()
+            case .indirect:
+                idi()
+            case .indirectX:
+                idx()
+            case .indirectY:
+                idy()
+        }
+
+        switch operation.instruction {
+            case .adc:
+                adc()
+            case .sbc:
+                sbc()
+            case .inc:
+                inc()
+            case .dec:
+                dec()
+            case .inx:
+                inx()
+            case .iny:
+                iny()
+            case .dex:
+                dex()
+            case .dey:
+                dey()
+            case .and:
+                and()
+            case .ora:
+                ora()
+            case .eor:
+                eor()
+            case .asl:
+                asl()
+            case .lsr:
+                lsr()
+            case .rol:
+                rol()
+            case .ror:
+                ror()
+            case .lda:
+                lda()
+            case .ldx:
+                ldx()
+            case .ldy:
+                ldy()
+            case .sta:
+                sta()
+            case .stx:
+                stx()
+            case .sty:
+                sty()
+            case .tax:
+                tax()
+            case .tay:
+                tay()
+            case .tsx:
+                tsx()
+            case .txa:
+                txa()
+            case .tya:
+                tya()
+            case .txs:
+                txs()
+            case .jmp:
+                jmp()
+            case .jsr:
+                jsr()
+            case .rts:
+                rts()
+            case .rti:
+                rti()
+            case .bcc:
+                bcc()
+            case .bcs:
+                bcs()
+            case .beq:
+                beq()
+            case .bne:
+                bne()
+            case .bmi:
+                bmi()
+            case .bpl:
+                bpl()
+            case .bvc:
+                bvc()
+            case .bvs:
+                bvs()
+            case .stp:
+                stp()
+            case .brk:
+                brk()
+            case .pha:
+                pha()
+            case .php:
+                php()
+            case .pla:
+                pla()
+            case .plp:
+                plp()
+            case .clc:
+                clc()
+            case .cld:
+                cld()
+            case .cli:
+                cli()
+            case .clv:
+                clv()
+            case .sec:
+                sec()
+            case .sed:
+                sed()
+            case .sei:
+                sei()
+            case .cmp:
+                cmp()
+            case .cpx:
+                cpx()
+            case .cpy:
+                cpy()
+            case .bit:
+                bit()
+            case .dop:
+                dop()
+            case .top:
+                top()
+            case .slo:
+                slo()
+            case .rla:
+                rla()
+            case .sre:
+                sre()
+            case .rra:
+                rra()
+            case .sax:
+                sax()
+            case .ahx:
+                ahx()
+            case .shx:
+                shx()
+            case .shy:
+                shy()
+            case .tas:
+                tas()
+            case .las:
+                las()
+            case .lax:
+                lax()
+            case .dcp:
+                dcp()
+            case .isc:
+                isc()
+            case .alr:
+                alr()
+            case .anc:
+                anc()
+            case .arr:
+                arr()
+            case .axs:
+                axs()
+            case .xaa:
+                xaa()
+            case .nop:
+                nop()
+        }
     }
 }
