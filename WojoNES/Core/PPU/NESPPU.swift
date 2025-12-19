7970e9d1a731c7172acb2989d1c1cd25bb284aa0
Wojciech Kosikowski <83952389+WKosikowski@users.noreply.github.com>
2025-12-19 16:47:58 +0000

    NESPPU private methods and properties

--- WojoNES/Core/PPU/NESPPU.swift
+++ WojoNES/Core/PPU/NESPPU.swift
@@ -10,6 +10,20 @@ class NESPPU: PPU {
 
     var bus: Bus!
 
+    var frameBuffer: [UInt32]
+
+    private var vram: [UInt8] = []
+
+    private var oam: [UInt8] = []
+
+    private var palette: [UInt8] = []
+
+    private var scanline: Int?
+
+    private var cycle: Int?
+
+//    private var controlRegister: ControlRegister
+
     // MARK: Lifecycle
 
     init() {}
@@ -19,4 +33,26 @@ class NESPPU: PPU {
     func connect(_ bus: Bus) {
         self.bus = bus
     }
+
+    func frameReady() -> Bool {
+        <#code#>
+    }
+
+    func step() {
+        <#code#>
+    }
+
+    func read(_ address: UInt16) -> UInt8 {
+        <#code#>
+    }
+
+    func write(address: UInt16, value: UInt8) {
+        <#code#>
+    }
+
+    private func renderScanline() {}
+
+    private func fetchBackgrounds() {}
+
+    private func fetchSprites() {}
 }
--- WojoNES/Core/PPU/PPU.swift
+++ WojoNES/Core/PPU/PPU.swift
@@ -5,12 +5,12 @@
 //  Created by Wojciech Kosikowski on 05/07/2025.
 //
 
-protocol PPU: AnyObject {
+protocol PPU {
     func connect(_ bus: Bus)
     func frameReady() -> Bool
     func step()
     func read(_ address: UInt16) -> UInt8
     func write(address: UInt16, value: UInt8)
 
-    var frameBuffer: [UInt32]
+    var frameBuffer: [UInt32] { get set }
 }
