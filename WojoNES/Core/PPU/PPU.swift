18e53a6bbdfdb1af602634510d8177729c43a82b
Wojciech Kosikowski <83952389+WKosikowski@users.noreply.github.com>
2025-12-19 16:28:37 +0000

    PPU Protocol Methods and Properties

--- WojoNES/Core/PPU/PPU.swift
+++ WojoNES/Core/PPU/PPU.swift
@@ -7,4 +7,10 @@
 
 protocol PPU: AnyObject {
     func connect(_ bus: Bus)
+    func frameReady() -> Bool
+    func step()
+    func read(_ address: UInt16) -> UInt8
+    func write(address: UInt16, value: UInt8)
+
+    var frameBuffer: [UInt32]
 }
