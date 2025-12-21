fd3dff58d91ce476fa4d7a1089097fe7ec2cfcf2
Wojciech Kosikowski <83952389+WKosikowski@users.noreply.github.com>
2025-12-21 10:42:06 +0000

    Create Point.swift

---
+++ WojoNES/Rendering/Point.swift
@@ -0,0 +1,20 @@
+//
+//  Point.swift
+//  WojoNES
+//
+//  Created by Wojciech Kosikowski on 21/12/2025.
+//
+
+struct Point {
+    // MARK: Properties
+
+    var x: Int
+    var y: Int
+
+    // MARK: Lifecycle
+
+    init(x: Int, y: Int) {
+        self.x = x
+        self.y = y
+    }
+}
